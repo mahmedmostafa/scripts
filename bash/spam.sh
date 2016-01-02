cf27527bdc751166f0721b1e771dfbe2836a2331
root <root@St0rM.local>
2016-01-02 14:44:32 +0200

    Enhancing

--- bash/spam.sh
+++ bash/spam.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Find location of spam on cPanel/exim server
 
-grep cwd /var/log/exim_mainlog | egrep -o "cwd=[^ ]*"  | awk -F'=' '{print $2}' | sort | uniq -c
+egrep -o "cwd=[^ ]*" /var/log/exim_mainlog  | awk -F'=' '{print $2}' | sort | uniq -c
